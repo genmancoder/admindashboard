a19209029ba31a8754108f8c6ee0d07a3deb8457
johnrpx <johnrp@simonenterprises.com>
2022-07-11 11:58:47 +0800

    react components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css'
+import App from './App'
 
+import {ContextProvider} from './contexts/ContextProvider'
 
+ReactDOM.render(
+    <ContextProvider>
+    <App/>
+    </ContextProvider>
+, document.getElementById('root'));
