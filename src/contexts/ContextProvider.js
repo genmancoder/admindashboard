6ea41cf43ae6d6d44f48e21434f3dd1890dead2a
johnrpx <johnrp@simonenterprises.com>
2022-07-11 22:23:43 +0800

    package.json edited

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -28,7 +28,7 @@ export const ContextProvider = ({children}) => {
       };
     
 
-    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: !isClicked });
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
 
     return (
         <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
