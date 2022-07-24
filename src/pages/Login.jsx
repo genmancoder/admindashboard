04b04b196a41c693d825a52fe5e4928a0dfb2eaa
= <=>
2022-07-25 02:48:40 +0800

    Added firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-const Register = () => {
+const Login = () => {
   return (
-    <div className="container mx-auto mt-10">
-      <h1 className="text-md dark:text-white mb-10">Sign Up</h1>
+    <div className="container mx-auto">
       <form>
         <div class="relative z-0 mb-6 w-full group">
             <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""/>
@@ -25,4 +24,4 @@ const Register = () => {
   )
 }
 
-export default Register
+export default Login
