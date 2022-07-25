276c8e8266314fb9b74cb1f240578b75e900042c
= <=>
2022-07-25 08:08:08 +0800

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Register = () => {
   return (
     <div className="container mx-auto mt-10">
-      <h1 className="text-md dark:text-white mb-10">Sign Up</h1>
+      <h1 className="text-md dark:text-white mb-10">Sign Up -</h1>
       <form>
         <div class="relative z-0 mb-6 w-full group">
             <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required=""/>
